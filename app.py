4aa470595b0c2216563f2bca2919f20f68141448
JuanDigital <juanperez1639@gmail.com>
2023-03-26 23:01:13 -0600

    control web a mqtt

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ from flask import Flask, render_template,request
 from publish import PubMqtt
 
 app=Flask(__name__)
-
+@app.route('/')
+def index():
+	data={'titulo':'Bienvenido','encabezado':'Controla tu bomba'}
+	return render_template('base.html',data=data)
 
 @app.route('/datos')
 def datos():
 	print(request.args)
 	status=request.args.get('accion')
 	enviar=PubMqtt()
-	enviar.respuesta('control',status)	
-	return 'la bomba se debe: {}'.format(status)
+	enviar.respuesta('control',status)
+	data={'titulo':'Bienvenido','encabezado':'Controla tu bomba'}
+	estado={'actual':status}	
+	return render_template('index.html',data=data,estado=estado)
 
 
 if __name__ =="__main__":
